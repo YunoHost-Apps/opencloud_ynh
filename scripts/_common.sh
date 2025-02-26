7906a68bb872b8c9c1646b072850c5f9785a3bf6
eric_G <46165813+ericgaspar@users.noreply.github.com>
2025-02-26 11:27:14 +0100

    first commit

---
+++ scripts/_common.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES AND CUSTOM HELPERS AND CUSTOM HELPERS
+#=================================================
+
+timezone=$(cat /etc/timezone)
